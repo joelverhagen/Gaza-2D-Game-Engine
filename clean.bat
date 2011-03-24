184aba52315aa7577bd978a325dcb2c0079b143f
Joel Verhagen <joel.verhagen@gmail.com>
2011-03-23 23:42:30 -0400

    Adjusted the clean.bat file to not delete the .suo file (basically, what files I have opened in Visual Studio) and included my .suo file.

--- clean.bat
+++ clean.bat
@@ -1,8 +1,7 @@
 del SetGame.sdf
-del SetGame.suo /A:H
 del SetGame\SetGame.vcxproj.user
 rmdir /Q /S Debug
-rmdir /Q /S ipch
 rmdir /Q /S Release
-rmdir /Q /S SetGame\Release
+rmdir /Q /S ipch
 rmdir /Q /S SetGame\Debug
+rmdir /Q /S SetGame\Release
