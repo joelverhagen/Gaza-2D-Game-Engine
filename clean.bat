614f437e88cc2382aa7341e81b6407c9a5404c52
Joel Verhagen <joel.verhagen@gmail.com>
2011-03-23 23:38:02 -0400

    Added a script to simplify deleting unneeded Visual Studio files.

---
+++ clean.bat
@@ -0,0 +1,8 @@
+del SetGame.sdf
+del SetGame.suo /A:H
+del SetGame\SetGame.vcxproj.user
+rmdir /Q /S Debug
+rmdir /Q /S ipch
+rmdir /Q /S Release
+rmdir /Q /S SetGame\Release
+rmdir /Q /S SetGame\Debug
